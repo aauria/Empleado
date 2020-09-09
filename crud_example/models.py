438f35cd4c3bfa41e4e44e01af58e9406e5b4d3e
aauria <afauria@est.itssg.edu.ec>
2020-09-09 03:09:56 -0500

    cuarto_respaldo

--- crud_example/models.py
+++ crud_example/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Empleado(models.Model):
-    nombre = models.CharField(max_length=100)
-    apellido= models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100,unique=True)
+    apellido= models.CharField(max_length=100,unique=True)
     fecha_naciento = models.DateField('YYYY-MM-DD')
-    direccion = models.CharField(max_length=100)
-    correo = models.EmailField()
-    telefono = models.CharField(max_length=15)
+    direccion = models.CharField(max_length=100,unique=True)
+    correo = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=15,unique=True)
 
     def __str__(self):
         return '{} {}',format(self.nombre, self.apellido)
@@ -17,12 +17,12 @@ class Empleado(models.Model):
         db_table = "empleado"
 
 class Cargo(models.Model):
-    nombre = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=50,unique=True)
+    descripcion = models.CharField(max_length=100,unique=True)
     Empleado = models.OneToOneField(Empleado, null=True, blank=True, on_delete=models.CASCADE)
 
 class Departamento(models.Model):
-    nombre = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100,unique=True)
     Empleado = models.ForeignKey(Empleado, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
